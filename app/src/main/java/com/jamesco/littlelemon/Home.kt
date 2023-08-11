b5d1168dac596ab2f50bb2f7015d0ad1b4c9295d
James Michael E. Paz <105730089+JmePaz@users.noreply.github.com>
2023-08-11 22:55:33 +0800

    Improve

--- app/src/main/java/com/jamesco/littlelemon/Home.kt
+++ app/src/main/java/com/jamesco/littlelemon/Home.kt
@@ -1,5 +1,6 @@
 package com.jamesco.littlelemon
 
+import android.util.Log
 import android.view.Menu
 import android.widget.Space
 import androidx.compose.foundation.Image
@@ -7,11 +8,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
@@ -25,7 +29,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.get
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
@@ -35,6 +41,8 @@ import com.jamesco.littlelemon.ui.theme.Primary1
 import com.jamesco.littlelemon.ui.theme.Secondary3
 import com.jamesco.littlelemon.ui.theme.karlaTypography
 import com.jamesco.littlelemon.ui.theme.marzakiTypography
+import io.ktor.http.*
+import java.util.*
 
 
 @Composable
@@ -46,20 +54,28 @@ fun Home(navController: NavHostController){
             AppDatabase::class.java, "Little-lemon-db"
         ).build()
     }
-
-    var filterList by remember {
+    var filterList by remember{
         mutableStateOf(listOf<MenuItem>())
     }
-
-
+    var categories by remember {
+        mutableStateOf(listOf<String>())
+    }
     val menuItemList by db.menuDao().getLiveMenuItems().observeAsState(listOf()).also {
-        filterList = it.value
+
+        categories = it.value.groupBy { menuItem -> menuItem.category }.keys.toList()
+            .map{it.replaceFirstChar {
+                it.titlecase(Locale.getDefault())
+            }}.plus("Sides")
+
     }
 
     var searchPhrase by remember{
         mutableStateOf("")
     }
 
+    var selectedCategory by remember{
+        mutableStateOf("")
+    }
 
 
     Column(modifier = Modifier.fillMaxSize()) {
@@ -122,13 +138,7 @@ fun Home(navController: NavHostController){
                 OutlinedTextField(value = searchPhrase,
                     onValueChange = {
                         searchPhrase = it
-                       if(searchPhrase.trim()!="") {
-                           filterList = menuItemList.filter { it.title
-                               .contains(searchPhrase, ignoreCase = true) }
-                       }
-                        else{
-                            filterList = listOf()
-                       }
+
                     },
                 leadingIcon = {Icon(Icons.Default.Search,"Search Icon")},
                 modifier = Modifier
@@ -144,6 +154,7 @@ fun Home(navController: NavHostController){
             }
 
         }
+
         Column(modifier = Modifier
             .weight(1f)
             .padding(horizontal = 10.dp)){
@@ -154,75 +165,88 @@ fun Home(navController: NavHostController){
                 .weight(0.3f)
                 .padding(start = 10.dp),
             style = karlaTypography.subtitle1)
-            Row(modifier = Modifier
+            LazyRow(modifier = Modifier
                 .fillMaxWidth()
                 .weight(0.8f),
             horizontalArrangement = Arrangement.SpaceAround) {
-                CategoryButton(category = "Starters") {
-                    if(searchPhrase.trim()==""){
-                        filterList = menuItemList.filter { it.category.equals("Starters", ignoreCase = true) }
-                    }
-                    else{
-                        filterList = filterList.filter { it.category.equals("Starters", ignoreCase = true) }
-                    }
-                }
-                CategoryButton(category = "Main") {
-                    if(searchPhrase.trim()==""){
-                        filterList = menuItemList.filter { it.category.equals("Mains", ignoreCase = true) }
-                    }
-                    else{
-                        filterList = filterList.filter { it.category.equals("Mains", ignoreCase = true) }
-                    }
-                }
-                CategoryButton(category = "Desserts") {
-                    if(searchPhrase.trim()==""){
-                        filterList = menuItemList.filter { it.category.equals("Desserts", ignoreCase = true) }
-                    }
-                    else{
-                        filterList = filterList.filter { it.category.equals("Desserts", ignoreCase = true) }
-                    }
-                }
-                CategoryButton(category = "Sides") {
-                    if(searchPhrase.trim()=="") {
-                        filterList = menuItemList.filter {
-                            it.category.equals(
-                                "Sides",
-                                ignoreCase = true
-                            )
-                        }
-                    }
-                    else{
-                        filterList = filterList.filter {
-                            it.category.equals(
-                                "Sides",
-                                ignoreCase = true
-                            )
-                        }
-                    }
-                }
+                items(
+                  items = categories,
+                  itemContent = { category ->
+                      CategoryButton(category = category,
+                          selectedCategory==category
+                      ) {
+                          selectedCategory = if(selectedCategory==category) "" else category
+                      }
+                  }
+                )
             }
             Spacer(modifier = Modifier.weight(0.2f))
             Divider(color = Secondary3, thickness = 2.dp)
         }
-        LazyColumn(modifier = Modifier.weight(3f,fill=true) ){
-            items(
-                items = filterList,
-                itemContent = {
-                    menuItemCard(menuItem = it)
-                }
-            )
+
+        //filtering process
+        filterList = when{
+            searchPhrase.trim()=="" && selectedCategory!=""->{
+                menuItemList.filter {
+                    it.category.equals(selectedCategory, ignoreCase = true) }
+            }
+            searchPhrase.trim()!=""&&selectedCategory==""->{
+                menuItemList.filter { menuItem -> menuItem.title
+                    .contains(searchPhrase, ignoreCase = true) }
+
+            }
+            searchPhrase.trim()!=""&& selectedCategory!=""->{
+                menuItemList.filter { it.category.equals(selectedCategory, ignoreCase = true) }
+                    .filter {
+                            menuItem -> menuItem.title
+                        .contains(searchPhrase, ignoreCase = true)
+                    }
+            }
+            else->{
+                menuItemList
+            }
+        }
+
+        //UI
+
+        if(filterList.isEmpty()){
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+                .weight(3f, fill = true),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center) {
+                Icon(Icons.Default.Close, "No information",
+                      modifier = Modifier.size(45.dp))
+                Text("No result found for `$searchPhrase` `$selectedCategory`",
+                style = karlaTypography.caption)
+            }
+
+        }
+        else {
+            LazyColumn(
+                modifier = Modifier.weight(3f, fill = true)
+            ) {
+
+                items(
+                    items = filterList,
+                    itemContent = {
+                        menuItemCard(menuItem = it)
+                    }
+                )
+            }
         }
     }
 }
 
 @Composable
-fun CategoryButton(category:String, onClick: ()->Unit){
+fun CategoryButton(category:String, isSelected:Boolean = false, onClick: ()->Unit){
     Button(onClick = onClick,
     shape = RoundedCornerShape(20.dp),
     colors = ButtonDefaults.buttonColors(
-        backgroundColor = Secondary3
+        backgroundColor = if (isSelected) Primary1 else Secondary3
     )) {
-        Text(category, color = Primary1,
+        Text(category, color = if(isSelected) Secondary3 else Primary1,
         style = karlaTypography.body2)
     }
 }
